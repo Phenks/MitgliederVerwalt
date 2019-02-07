258cd2acc1bfb7b7ece6897022a29237fb15edae
Philipp Henke <philipp.henke98@outlook.de>
2019-02-07 09:55:56 +0100

    Add project files.

---
+++ MitgliederVerwaltung/Views/Hauptfenster/HauptFenster.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace MitgliederVerwaltung.Views.Hauptfenster
+{
+    /// <summary>
+    /// Interaction logic for HauptFenster.xaml
+    /// </summary>
+    public partial class HauptFenster : Window
+    {
+        public HauptFenster()
+        {
+            var viewModel = new HauptFensterViewModel();
+            viewModel.Name = "Test";
+            DataContext = viewModel;
+            
+            InitializeComponent();
+        }
+    }
+}
---
+++ MitgliederVerwaltung/Views/Hauptfenster/HauptFensterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Input;
+
+namespace MitgliederVerwaltung.Views.Hauptfenster
+{
+    class HauptFensterViewModel : ViewModelBasis
+    {
+
+        private readonly DelegateCommand _changeNameCommand;
+        public ICommand ChangeNameCommand => _changeNameCommand;
+
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+        public HauptFensterViewModel()
+        {
+            _changeNameCommand = new DelegateCommand(OnChangeName,CanChangeName);
+        }
+
+        private bool CanChangeName(object commandParameter)
+        {
+            return Name != "Drölf";
+        }
+
+        private void OnChangeName(object commandParameter)
+        {
+            Name = "Drölf";
+            _changeNameCommand.InvokeCanExecuteChanged();
+        }
+
+        
+    }
+}
